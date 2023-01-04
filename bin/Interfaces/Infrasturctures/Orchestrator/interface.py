a53cd1e2a4ee23beb87395aa9b67c29fb99945ce
Mohammad Rimawi <mohrimawiz@gmail.com>
2023-01-05 00:57:32 +0300

    Adding support of list operations in orchestrator interface

--- bin/Interfaces/Infrasturctures/Orchestrator/interface.py
+++ bin/Interfaces/Infrasturctures/Orchestrator/interface.py
@@ -1,26 +1,66 @@
+from typing import List
+
 import typer
+from Models import ServicesFilter
 
 from .main import OrchestratorInfrastructureClient as OIC
-
 
 orchestrator = typer.Typer(add_completion=True)
 
 
 @orchestrator.command("up")
-def orchestrator_up(service_name: str):
-    print(OIC.up(service_name))
+def orchestrator_up(
+    services_names: List[str],
+):
+    for status in OIC.up(
+        ServicesFilter(
+            services_names=services_names,
+            select_all=services_names[0].lower() == "all",
+        )
+    ):
+        print(status)
 
 
 @orchestrator.command("down")
-def orchestrator_down(service_name: str):
-    print(OIC.down(service_name))
+def orchestrator_down(
+    services_names: List[str],
+):
+    for status in OIC.down(
+        ServicesFilter(
+            services_names=services_names,
+            select_all=services_names[0].lower() == "all",
+        )
+    ):
+        print(status)
 
 
 @orchestrator.command("restart")
-def orchestrator_restart(service_name: str):
-    print(OIC.restart(service_name))
+def orchestrator_restart(
+    services_names: List[str],
+):
+    for status in OIC.restart(
+        ServicesFilter(
+            services_names=services_names,
+            select_all=services_names[0].lower() == "all",
+        )
+    ):
+        print(status)
 
 
 @orchestrator.command("status")
-def orchestrator_status(service_name: str):
-    print(OIC.status(service_name))
+def orchestrator_status(
+    services_names: List[str],
+):
+    for status in OIC.status(
+        ServicesFilter(
+            services_names=services_names,
+            select_all=services_names[0].lower() == "all",
+        )
+    ):
+        print(status)
+
+
+@orchestrator.command("list")
+def orchestrator_list():
+    for service_name in OIC.list():
+        print(service_name)
--- bin/Interfaces/Infrasturctures/Orchestrator/main.py
+++ bin/Interfaces/Infrasturctures/Orchestrator/main.py
@@ -1,36 +1,49 @@
 import requests
 from conf import settings
+from Models import ServicesFilter
 
 
 class OrchestratorInfrastructureClient:
     @staticmethod
-    def up(service_name: str):
-        response = requests.get(
-            f"{settings.GATEWAY_URL}/up/{service_name}",
+    def up(service_filter: ServicesFilter):
+        response = requests.post(
+            f"{settings.GATEWAY_URL}/up",
+            json=service_filter.dict(),
             timeout=3,
         )
         return response.json()
 
     @staticmethod
-    def down(service_name: str):
-        response = requests.get(
-            f"{settings.GATEWAY_URL}/down/{service_name}",
+    def down(service_filter: ServicesFilter):
+        response = requests.post(
+            f"{settings.GATEWAY_URL}/down",
+            json=service_filter.dict(),
             timeout=3,
         )
         return response.json()
 
     @staticmethod
-    def restart(service_name: str):
-        response = requests.get(
-            f"{settings.GATEWAY_URL}/restart/{service_name}",
+    def restart(service_filter: ServicesFilter):
+        response = requests.post(
+            f"{settings.GATEWAY_URL}/restart",
+            json=service_filter.dict(),
             timeout=6,
         )
         return response.json()
 
     @staticmethod
-    def status(service_name: str):
-        response = requests.get(
-            f"{settings.GATEWAY_URL}/status/{service_name}",
+    def status(service_filter: ServicesFilter):
+        response = requests.post(
+            f"{settings.GATEWAY_URL}/status",
+            json=service_filter.dict(),
+            timeout=3,
+        )
+        return response.json()
+
+    @staticmethod
+    def list():
+        response = requests.post(
+            f"{settings.GATEWAY_URL}/list",
             timeout=3,
         )
         return response.json()
