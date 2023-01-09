d40ff83a82d0c859995fd87e57e529b26a1f7ac5
Mohammad Rimawi <mohrimawiz@gmail.com>
2023-01-09 21:22:16 +0300

    Adding support for binding to notification core channel #3

--- bin/Interfaces/Cores/Notification/interface.py
+++ bin/Interfaces/Cores/Notification/interface.py
@@ -11,3 +11,12 @@ notification = typer.Typer(add_completion=True)
 @notification.command("push")
 def push(channel_name: str, message: str):
     Notify.push(NotificationMessage(channel_name=channel_name, message=message))
+
+
+@notification.command("bind")
+def bind(channel_name: str):
+    try:
+        Notifications.bind(channel_name)
+    except Exception as e:
+        print(e)
+        pass
--- bin/Interfaces/Cores/Notification/main.py
+++ bin/Interfaces/Cores/Notification/main.py
@@ -1,5 +1,7 @@
 import requests
 from conf import settings
+from VarlaLib.Shell import VarlaCLI as Varla
+from websocket import create_connection
 
 
 class Notifications:
@@ -9,3 +11,17 @@ class Notifications:
             f"{settings.GATEWAY_URL}/push/{channel_name}/{message}", timeout=3
         )
         return response.json()
+
+    @staticmethod
+    def bind(channel_name: str):
+        Varla.clear(top_text="CHANNEL", bottom_text=channel_name)
+        try:
+            ws = create_connection(
+                f"ws://{settings.NOTIFICATION_CORE_URL}/bind/{channel_name}"
+            )
+            while True:
+                Varla.say(ws.recv(), name=channel_name)
+
+        except:
+            ws.close()
+            Varla.clear()
